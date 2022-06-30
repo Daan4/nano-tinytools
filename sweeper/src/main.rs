86fd0b68b983f0baba280e723091fead12725a9a
Daan4 <Daan4@users.noreply.github.com>
2022-06-30 18:35:04 +0200

    add multithreading

--- sweeper/src/main.rs
+++ sweeper/src/main.rs
@@ -1,9 +1,12 @@
 use serde_derive::Deserialize;
 use std::fs;
-use std::time::Instant;
+use std::time::{Instant, Duration};
 use toml;
 use lazy_static::lazy_static;
 use nano_tinytools_common::{derive_private_key, derive_public_key, derive_address, hexstring_to_bytes, validate_seed, validate_address};
+use std::sync::mpsc::{channel, Receiver, Sender};
+use std::thread;
+use crossbeam_deque::{Stealer, Steal, Worker};
 
 lazy_static! {
     static ref CONFIG: Config = Config::new();
@@ -17,6 +20,7 @@ struct Config {
     target: String,
     start_index: u32,
     stop_index: u32,
+    num_threads: u32,
 }
 
 impl Config { 
@@ -32,7 +36,8 @@ fn main() {
     // Read config
     let seed = CONFIG.seed.to_owned();
     let target = CONFIG.target.to_owned();
-    let mut index = CONFIG.start_index;
+    let num_threads = CONFIG.num_threads;
+    let start_index = CONFIG.start_index;
     let stop_index = CONFIG.stop_index;
 
     // Validate seed and target format
@@ -45,23 +50,56 @@ fn main() {
         panic!("Target must be a valid nano address")
     }
 
-    // Sweep
-    while index < stop_index {
-        let private_key = derive_private_key(seed, index);
-        let public_key = derive_public_key(private_key);
-        let address = derive_address(public_key);
+    let q = Worker::new_fifo();
 
-        if address == target {
-            println!("\nfound target {} @ index {}", address, index);
-            break;
-        }
-
-        index += 1;
-
-        if index % 1000 == 0 {
-            println!("{}", index)
-        }
+    let (tx, rx): (Sender<()>, Receiver<()>) = channel();
+    for id in 0..num_threads {
+        let tx_clone = tx.clone();
+        let s = q.stealer();
+        thread::spawn(move || {
+            consumer(id, tx_clone, s, seed);
+        });
     }
 
+    thread::spawn(move || {
+        producer(q, start_index, stop_index);
+    });
+
+    rx.recv().unwrap();
     println!("\ntime taken: {} s", now.elapsed().as_millis() as f32 / 1000.0);
+}
+
+fn producer(queue: Worker<u32>, start_index: u32, stop_index: u32) {
+    for index in start_index..stop_index {
+        queue.push(index);
+        if queue.len() > 10000 {
+            thread::sleep(Duration::new(0, 100000000));
+        }
+    }
+}
+
+fn consumer(id: u32, tx: Sender<()>, queue: Stealer<u32>, seed: [u8; 32]) {
+    let target = CONFIG.target.to_owned();
+    let mut count = 0;
+    loop {
+        match queue.steal() {
+            Steal::Success(index) => {
+                let private_key = derive_private_key(seed, index);
+                let public_key = derive_public_key(private_key);
+                let address = derive_address(public_key);
+        
+                if address == target {
+                    println!("\nfound target {} @ index {}", address, index);
+                    tx.send(()).unwrap();
+                    break;
+                }
+        
+                count += 1;
+                if count % 1000 == 0 {
+                    println!("thread {} count {}", id, count);
+                }
+            },
+            _ => {}
+        }
+    }
 }
